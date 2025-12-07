98c374bc615ae71d452b654773e4fb43a5288d2f
Pallavi Anil Sindkar <83487183+Pallavi-S-02@users.noreply.github.com>
2025-12-07 21:14:16 +0530

    Add files via upload

---
+++ rag1.py
@@ -0,0 +1,4 @@
+# Advanced Q&A Chatbot Using Ragstack With vector-enabled Astra DB Serverless database And Huggingface 
+#RAG Stack, AstraDB - explore this more -> do one own separate projects using this
+
+# langchain agents read from documentation also see opensource models agents like gemini
